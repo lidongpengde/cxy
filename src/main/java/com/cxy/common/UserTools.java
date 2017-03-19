9f771114ca68c214908cfe1a7e3602c484977fd7
lidp <821888642@qq.com>
2017-03-19 15:22:56 +0800

    0319

---
+++ src/main/java/com/cxy/common/UserTools.java
@@ -0,0 +1,13 @@
+package com.cxy.common;
+
+import com.cxy.entity.User;
+
+/**
+ * Created by lidp on 2017/3/19.
+ */
+public class UserTools {
+    public static User getCurrentUser(){
+        User user=new User();
+        return user;
+    }
+}
